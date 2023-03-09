fa75de56f2dc4b15d2bb364f6786810221b68fed
xing-6464 <xing_6464@foxmail.com>
2023-03-09 21:28:10 +0800

    添加注释

--- Algorithm/Search/LinearSearch/LinearSearch.ts
+++ Algorithm/Search/LinearSearch/LinearSearch.ts
@@ -1,8 +1,21 @@
+/**
+ * @description 线性查找算法
+ * @author 星光
+ */
+
 class LinearSearch {
   private constructor() {}
 
-  static search<T>(data: T[], target: T): number {
+  static search<T>(
+    data: T[],
+    target: T,
+    fn?: (n1: T, n2: T) => boolean
+  ): number {
     for (let i = 0, len = data.length; i < len; i++) {
+      if (fn) {
+        if (fn(data[i], target)) return i
+        continue
+      }
       if (data[i] === target) return i
     }
 
