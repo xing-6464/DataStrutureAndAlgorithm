3295c483cc97025b91c5fdd661839553c0d583fa
xing-6464 <xing_6464@foxmail.com>
2023-03-09 21:07:53 +0800

    test linearsearch

--- Algorithm/Search/LinearSearch/LinearSearch.test.ts
+++ Algorithm/Search/LinearSearch/LinearSearch.test.ts
@@ -1,9 +1,23 @@
 import LinearSearch from './LinearSearch'
 
-test('test LinearSearch', () => {
+const setup = (i: number) => {
   const data = [24, 18, 12, 9, 16, 66, 32, 4]
   const ls = new LinearSearch()
-  const res = ls.search(data, 16)
+  const res = ls.search(data, i)
 
-  expect(res).toBe(4)
+  return res
+}
+
+describe('测试 线性查找算法', () => {
+  test('能找到item', () => {
+    const res = setup(16)
+
+    expect(res).toBe(4)
+  })
+
+  test('找不到item', () => {
+    const res = setup(5)
+
+    expect(res).toBe(-1)
+  })
 })
