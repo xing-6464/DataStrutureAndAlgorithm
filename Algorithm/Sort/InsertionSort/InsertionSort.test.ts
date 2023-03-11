d6b43e600fe92fab87de94d70cd780a7a71f0355
xing-6464 <xing_6464@foxmail.com>
2023-03-11 08:00:07 +0800

    实现插入排序算法

---
+++ Algorithm/Sort/InsertionSort/InsertionSort.test.ts
@@ -0,0 +1,11 @@
+import { InsertionSort } from './InsertionSort'
+
+describe('测试插入排序算法', () => {
+  test('正常情况', () => {
+    const arr = [6, 5, 4, 3, 2, 1]
+
+    InsertionSort.sort(arr)
+
+    expect(arr).toEqual([1, 2, 3, 4, 5, 6])
+  })
+})
---
+++ Algorithm/Sort/InsertionSort/InsertionSort.ts
@@ -0,0 +1,50 @@
+/**
+ * @description 插入排序算法
+ * @author 星光
+ */
+
+import { ArrayGenerator } from '../../util/ArrayGenerator'
+import { SortingHelper } from '../../util/SortingHelper'
+
+export class InsertionSort {
+  private constructor() {}
+
+  static sort<T>(arr: T[], fn?: (arr: T[], index: number) => number) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+      for (let j = i; j > 0; j--) {
+        if (fn) {
+          const res = fn(arr, j)
+          if (res < 0) {
+            InsertionSort.swap(arr, j)
+            continue
+          }
+        }
+
+        if (arr[j - 1] > arr[j]) {
+          InsertionSort.swap(arr, j)
+        }
+      }
+    }
+  }
+
+  private static swap<T>(arr: T[], index: number) {
+    const mid = arr[index - 1]
+    arr[index - 1] = arr[index]
+    arr[index] = mid
+  }
+}
+
+/**
+ * 性能测试
+ */
+
+// function test() {
+//   const dataSize = [10000, 100000]
+
+//   for (const n of dataSize) {
+//     const arr = ArrayGenerator.generateRandomArray(n, n)
+//     SortingHelper.sortTest('InsertionSort', arr)
+//   }
+// }
+
+// test()
