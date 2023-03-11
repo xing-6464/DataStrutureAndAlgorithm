7f28889269246ca10bc6ba3094cf8ff02bbf2c46
xing-6464 <xing_6464@foxmail.com>
2023-03-11 10:46:03 +0800

    数据类实现添加元素功能

--- DataStruture/Array/MyArray.ts
+++ DataStruture/Array/MyArray.ts
@@ -1,3 +1,8 @@
+/**
+ * @description 数据结构 数组
+ * @author 星光
+ */
+
 export class MyArray {
   private _data: number[]
   private _size: number
@@ -9,6 +14,29 @@ export class MyArray {
       this._data = new Array(capacity)
     }
     this._size = 0
+  }
+
+  add(index: number, e: number) {
+    if (this._size === this._data.length)
+      new Error('insert failed, Array is full')
+
+    if (index < 0 || index > this._size)
+      new Error('Add failed. Require index >= 0 and index <= size.')
+
+    for (let i = this._size; i > index; i--) {
+      this._data[i] = this._data[i - 1]
+    }
+
+    this._data[index] = e
+    this._size++
+  }
+
+  addLast(e: number) {
+    this.add(this._size, e)
+  }
+
+  addFirst(e: number) {
+    this.add(0, e)
   }
 
   isEmpty() {
