5386e296ac057a878d2e52de3b314f99bc8060a0
xing-6464 <xing_6464@foxmail.com>
2023-03-12 18:40:20 +0800

    完成栈数据结构测试

---
+++ DataStruture/Stack/Stack.test.ts
@@ -0,0 +1,25 @@
+import { Stack } from './Stack'
+
+describe('测试 栈数据结构', () => {
+  it('测试入栈', () => {
+    const stack = new Stack<number>()
+    for (let i = 0; i < 5; i++) {
+      stack.push(i)
+    }
+
+    expect(stack.size).toBe(5)
+    expect(stack.peek()).toBe(4)
+  })
+
+  it('测试出栈', () => {
+    const stack = new Stack<number>()
+    for (let i = 0; i < 5; i++) {
+      stack.push(i)
+    }
+
+    const res = stack.pop()
+    expect(res).toBe(4)
+    expect(stack.size).toBe(4)
+    expect(stack.peek()).toBe(3)
+  })
+})
